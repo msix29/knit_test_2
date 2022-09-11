ca42ca76b58387c704f3b16c0687ae56db24c4a3
msix29 <abdulrehmanessam7567@gmail.com>
2022-09-12 02:34:27 +0300

    finished

--- src/ServerStorage/services/placementService.lua
+++ src/ServerStorage/services/placementService.lua
@@ -3,7 +3,6 @@ local players = game:GetService("Players")
 local serverStorage = game:GetService("ServerStorage")
 
 local knit = require(replicatedStorage.packages.Knit)
-local dataStore2 = require(serverStorage.modules.dataStore2)
 
 local placementService = knit.CreateService{
     Name = "placementService",
@@ -12,19 +11,31 @@ local placementService = knit.CreateService{
         destroyBlock = knit.CreateSignal()
     }
 }
+
+local function _snipToGrid(position, grid)
+    return Vector3.new(
+        (math.floor(position.X / grid) * grid) + grid / 2,
+        (math.floor(position.Y / grid) * grid) + grid / 2,
+        (math.floor(position.Z / grid) * grid) + grid / 2
+    )
+end
+
 local function _placeBlock(block, position)
     --was going for fancy animations but didn't see a need for it.
     block = block:Clone()
+    block.Anchored = true
+    block.CanCollide = true
     block.Parent = workspace
-    block.Position = position
+    block.Position = _snipToGrid(position, 2)
+    block.Orientation = Vector3.new()
 end
 
 function placementService:KnitInit()
-    self.Client.placeBlock:Connect(function(block, position)
+    self.Client.placeBlock:Connect(function(player, block, position)
         _placeBlock(block, position)
     end)
 
-    self.Client.destroyBlock:Connect(function(block)
+    self.Client.destroyBlock:Connect(function(player, block)
         block:Destroy()
     end)
 end
--- src/StarterGui/handler.client.lua
+++ src/StarterGui/handler.client.lua
@@ -1,1 +1,38 @@
 local players = game:GetService("Players")
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local runService = game:GetService("RunService")
+
+local player = players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local mouse = player:GetMouse()
+
+-- local function _snipToGrid(position, grid)
+--     return Vector3.new(
+--         (math.floor(position.X / grid) * grid) + grid / 2,
+--         (math.floor(position.Y / grid) * grid) + grid / 2,
+--         (math.floor(position.Z / grid) * grid) + grid / 2
+--     )
+-- end
+
+local knit = require(replicatedStorage.packages.Knit)
+
+knit.Start()
+
+local placementService = knit.GetService("placementService")
+
+character.ChildAdded:Connect(function(child)
+    if child:IsA("Tool") then
+        local clone = child.Handle:Clone()
+        clone.Parent = workspace
+        clone.Transparency = .8
+
+        -- runService.RenderStepped:Connect(function()
+        --     clone.Orientation = Vector3.new(0, 0, 0)
+        --     clone.Position = _snipToGrid(mouse.Hit.Position, 2)
+        -- end)
+
+        child.Activated:Connect(function()
+            placementService.placeBlock:Fire(child.Handle, mouse.Hit.Position)
+        end)
+    end
+end)
